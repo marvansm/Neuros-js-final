b312d1e33d8e59a7b36a9f3a91659d0900f7402c
Marvan <marvansm@code.edu.az>
2025-10-03 02:37:37 +0400

    fix: fix navbar

--- client/src/Pages/blog.js
+++ client/src/Pages/blog.js
@@ -6,41 +6,42 @@ const blogPage = () => {
   api.getData(`blogs?populate=*`).then((data) => {
     let renderHTML = data?.data
       ?.map(
-        (item) => `  <div class="rounded-[25px] overflow-hidden cursor-pointer">
-            <div class="image relative">
-              <img
-                src="http://localhost:1337${item?.img?.url}"
-                alt=""
-                class="w-full h-full object-cover"
-              />
+        (item) => ` <div class="rounded-[25px] overflow-hidden cursor-pointer">
+  <div class="image relative">
+    <img
+      src="http://localhost:1337${item?.img?.url}"
+      alt="${item?.title}"
+      class="w-full h-[220px] sm:h-[280px] md:h-[320px] lg:h-[380px] object-cover"
+    />
 
-              <div
-                class="bg-[#ffffff] rounded-t-[25px] px-[24px] pb-[3px] pt-[10px] absolute -bottom-1 left-0.5"
-              >
-                <h2
-                  class="leading-[2.1428em] font-medium text-[14px] text-[#9b9b9b]"
-                >
-                  5 Jan 2024 / andrew
-                </h2>
-              </div>
-            </div>
+    <div
+      class="bg-white rounded-t-[20px] px-4 pb-1 pt-2 absolute -bottom-1 left-1"
+    >
+      <h2
+        class="leading-[1.8em] font-medium text-[12px] sm:text-[13px] md:text-[14px] text-[#9b9b9b]"
+      >
+        5 Jan 2024 / andrew
+      </h2>
+    </div>
+  </div>
 
-            <div
-              class="body px-[40px] border border-gray-300 rounded-b-[25px] pb-[28px]"
-            >
-              <h2
-                class="text-[25px] leading-[35px] mt-[28px] tracking-[-.03em] text-[#111111] font-normal font-sora w-[350px]"
-              >
-               ${item?.title}
-              </h2>
+  <div
+    class="body px-4 sm:px-6 md:px-8 border border-gray-200 rounded-b-[25px] pb-6"
+  >
+    <h2
+      class="text-[18px] sm:text-[20px] md:text-[22px] lg:text-[25px] leading-[26px] sm:leading-[30px] md:leading-[34px] lg:leading-[35px] mt-6 tracking-[-.03em] text-[#111111] font-normal font-sora"
+    >
+      ${item?.title}
+    </h2>
 
-              <span
-                class="flex items-center gap-[3px] text-[#9b9b9b] text-[14px] leading-[2.1428em] font-medium mt-[22px]"
-              >
-            /${item?.tags} /
-              </span>
-            </div>
-          </div>`
+    <span
+      class="flex flex-wrap items-center gap-1 text-[#9b9b9b] text-[12px] sm:text-[13px] md:text-[14px] leading-[1.8em] font-medium mt-4"
+    >
+      /${item?.tags} /
+    </span>
+  </div>
+</div>
+`
       )
       .join("");
     BLOG_WRAPPER && (BLOG_WRAPPER.innerHTML = renderHTML);
